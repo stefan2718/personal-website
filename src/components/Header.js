e9106aec978450fd1b7783d006d5ea868af35ef8
Stefan Battiston <stefan.battiston@slalom.com>
2019-04-28 12:14:39 -0400

    2-space indents instead of 4.

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,20 +4,21 @@ import Footer from './Footer'
 import avatar from '../assets/images/avatar.jpg'
 
 class Header extends React.Component {
-    render() {
-        return (
-            <header id="header">
-                <div className="inner">
-                    <a href="#" className="image avatar"><img src={avatar} alt="" /></a>
-                    <h1><strong>I am Strata</strong>, a GatsbyJS starter
-                    <br />Crafted by <a href="http://html5up.net">HTML5 UP</a>.
-                    <br />Enhanced by <a href="https://appseed.us">AppSeed</a>.
-                    </h1>
-                </div>
-                <Footer />
-            </header>
-        )
-    }
+  render() {
+    return (
+      <header id="header">
+        <div className="inner">
+          <a href="#" className="image avatar"><img src={avatar} alt="" /></a>
+          <h1>
+            <strong>My name's Stefan</strong>, I like to build things.
+            <br />Crafted by <a href="http://html5up.net">HTML5 UP</a>.
+            <br />Enhanced by <a href="https://appseed.us">AppSeed</a>.
+          </h1>
+        </div>
+        <Footer />
+      </header>
+    )
+  }
 }
 
 export default Header
