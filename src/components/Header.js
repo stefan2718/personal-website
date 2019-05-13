2376c218d608f6656050a78a87f0ae081487b171
Stefan Battiston <stefan.battiston@gmail.com>
2019-05-13 09:59:26 -0400

    Work on shrinking header for blog/lab

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,23 +3,43 @@ import { Link } from 'gatsby'
 
 import Footer from './Footer'
 import avatar from '../assets/images/avatar.jpg'
+import { Location } from '@reach/router';
+
+const routes = [
+  { path: "/",      label: "Home" },
+  { path: "/blog",  label: "Blog" },
+  { path: "/lab",   label: "Lab" },
+];
 
 class Header extends React.Component {
   // TODO: Shrink this on blog/lab pages
+
+  getRoutes = (location) => {
+    let locationBase = /\/\w*/.exec(location.pathname)[0];
+    return routes
+      .filter(route => locationBase !== route.path)
+      .map(route => (
+        <div key={route.path}>
+          <Link to={route.path} className="button special">{route.label}</Link>
+        </div>
+      ));
+  }
+
   render() {
     return (
       <header id="header">
         <div className="inner">
           <Link to="/" className="no-underline" title="Go to homepage">
-            <img className="image avatar" src={avatar} alt="" />
+            <img className="image avatar" src={avatar} alt="Stefan" title="Wow, this is me!"/>
           </Link>
           <h1>
             <strong>My name's Stefan.</strong>
           </h1>
           <h1>I like to build things.</h1>
           <nav>
-            <Link to="/blog" className="button special">Blog</Link>
-            <Link to="/lab" className="button special">Lab</Link>
+            <Location>
+              { ({location}) => this.getRoutes(location) }
+            </Location>
           </nav>
         </div>
         <Footer />
