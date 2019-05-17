61061f28f4eb4fc3b7a4d61955feecc09e2168ab
Stefan Battiston <stefan.battiston@gmail.com>
2019-05-17 16:13:32 -0400

    Fix blog summary title styles.

--- src/components/BlogSummary.js
+++ src/components/BlogSummary.js
@@ -4,11 +4,11 @@ import { Link } from 'gatsby'
 export default function Template(props) {
   return (
     <article style={{marginBottom:'50px'}}>
-      <div style={{display:'flex', marginBottom:'10px'}}>
-        <span style={{flex:'1 0 auto', fontSize:'130%'}}>
+      <div style={{display:'flex', justifyContent: 'space-between', marginBottom:'10px'}}>
+        <span style={{flex:'1 1 auto', fontSize:'130%'}}>
           <Link to={props.path}>{props.title}</Link>
         </span>
-        <time dateTime={props.dateISO}>{props.date}</time>
+        <time style={{ flex:'0 0 auto', marginLeft: '1em' }} dateTime={props.dateISO}>{props.date}</time>
       </div>
       <summary>{props.excerpt}</summary>
     </article>
