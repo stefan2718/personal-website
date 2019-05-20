96fa3d16a2adfe4e9034acb1c9d4807ca3945813
Stefan Battiston <stefan.battiston@gmail.com>
2019-05-20 11:32:57 -0400

    Update manifest with proper name/colours.

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,14 +9,14 @@ module.exports = {
     'gatsby-plugin-catch-links',
     'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Stefan Battiston\'s site',
+        short_name: 'Stefan',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
+        background_color: '#49bf9d',
+        theme_color: '#49bf9d',
+        display: 'standalone',
         icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
       },
     },
