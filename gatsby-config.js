a893480295a03babe74dd01d0f20c4a3a422f724
Stefan Battiston <stefan.battiston@gmail.com>
2019-05-24 17:22:11 -0400

    Add purgecss to remove extraneous css.

--- gatsby-config.js
+++ gatsby-config.js
@@ -61,6 +61,17 @@ module.exports = {
       },
     },
     'gatsby-plugin-sass',
+    { 
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        // printRejected: true, // Print removed selectors and processed file names
+        develop: true, // Enable while using `gatsby develop`
+        // tailwind: true, // Enable tailwindcss support
+        // whitelist: ['whitelist'], // Don't remove this selector
+        ignore: ['prismjs/'], // Ignore files/folders
+        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+      }
+    },
     'gatsby-plugin-offline'
   ],
 }
