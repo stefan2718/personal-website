711707189172691a26db50254357dcbae64f2645
Stefan Battiston <stefan.battiston@gmail.com>
2019-05-27 22:29:47 -0400

    Add/test initial WebAssembly MarkerClusterer on Lab page.

--- src/pages/lab.js
+++ src/pages/lab.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-
 import HomePageLayout from '../components/HomePageLayout'
 
+const js = import("@stefan2718/webassembly-marker-clusterer");
+
 class Lab extends React.Component {
+  clusterer = null;
+
+  componentDidMount() {
+    js.then(lib => {
+      this.clusterer = lib;
+    });
+  }
+
+  greet = (str) => {
+    this.clusterer.greet(str);
+  }
 
   constructor() {
     super();
@@ -25,7 +37,8 @@ class Lab extends React.Component {
         <div id="main">
           <h1>The Lab</h1>
           <main>
-            Nothing here yet.
+            <h3>WebAssembly Experiment</h3>
+            <button className="button" onClick={() => this.greet('and the Wasm module calls Javascript\'s alert() function')}>This button calls a function in a Wasm module.</button>
           </main>
         </div>
       </HomePageLayout>
