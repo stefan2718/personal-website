5bc2513f3216484837a22990e9db737fa08574ef
Stefan Battiston <stefan.battiston@gmail.com>
2019-10-01 11:21:17 -0300

    Optimize images and fonts.

--- src/components/LabRoutes.tsx
+++ src/components/LabRoutes.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import wasmImg from '../assets/images/marker-clusterer-plus-example.png';
+import wasmImg from '../assets/images/marker-clusterer-plus-example.jpg';
 import LabTile from './LabTile';
 
 const labTiles = [
