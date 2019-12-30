20b6f5f48a2b7616ee9c399640c6f12cef5d01ff
Stefan Battiston <stefan.battiston@gmail.com>
2019-12-29 19:31:19 -0500

    Graph style adjustments.

--- src/components/lab/Graph.tsx
+++ src/components/lab/Graph.tsx
@@ -6,6 +6,9 @@ import './Graph.scss';
 import { combineTestResults } from '../../util/helpers';
 import { LOCAL_RESULTS_KEY_MCP, LOCAL_RESULTS_KEY_WASM } from '../../util/constants';
 
+const mcpColor = "#a62929";
+const wasmColor = "#1313d9";
+
 const divideResultsPerCluster = (results: ITestSummary[]): Partial<ITestSummary>[] => {
   if (!results) {
     return [];
@@ -102,15 +105,15 @@ export const Graph: React.FC<IGraphProps> = (props) => {
         </span>
       </div>
       <ResponsiveContainer className="graph">
-        <ComposedChart data={data} margin={{ top: 30, right: 30, bottom: 30, left: 30 }}>
+        <ComposedChart data={data} margin={{ top: 30, right: 30, bottom: 30, left: 45 }}>
           <Legend verticalAlign="top"/>
           <XAxis dataKey="newMarkersClustered" label={{ value: "New markers clustered (per cluster)", position: "bottom" }}
             type="number" domain={[0, 'dataMax']} allowDecimals={false} allowDataOverflow={true} padding={{ right: 10, left: 0}}/>
           <YAxis label={{ value: "Cluster time (per cluster)", angle: -90, position: "left", dy: -80, dx: -10 }} unit="ms" type="number"/>
-          <Scatter name="MCP" dataKey="mcpClusterTime" fill="red" shape={<Dot r={2}/>}/>
-          <Scatter name="Wasm" dataKey="wasmClusterTime" fill="blue" shape={<Dot r={2}/>}/>
-          <Line dataKey="wasm" stroke="blue" dot={false} legendType="none" strokeWidth="2"/>
-          <Line dataKey="mcp" stroke="red" dot={false} legendType="none" strokeWidth="2"/>
+          <Scatter name="MCP" dataKey="mcpClusterTime" fill={mcpColor} shape={<Dot r={2}/>}/>
+          <Scatter name="Wasm" dataKey="wasmClusterTime" fill={wasmColor} shape={<Dot r={2}/>}/>
+          <Line dataKey="wasm" stroke={wasmColor} dot={false} legendType="none" strokeWidth="2"/>
+          <Line dataKey="mcp" stroke={mcpColor} dot={false} legendType="none" strokeWidth="2"/>
         </ComposedChart>
       </ResponsiveContainer>
       <section id="equations">
